4d84f0547589243afb92e4bee09abfddc38c1bc0
oguhiro <166776874+oguhiro@users.noreply.github.com>
2024-06-14 12:50:51 +0900

    index

--- main.py
+++ main.py
@@ -17,22 +17,6 @@ async def root():
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
 
-    @app.get("/omikuji")
-def omikuji():
-    omikuji_list = [
-        "大吉",
-        "中吉",
-        "小吉",
-        "吉",
-        "半吉",
-        "末吉",
-        "末小吉",
-        "凶",
-        "小凶",
-        "大凶"
-    ]
-
-    return random.choice(omikuji_list)
 
     @app.get("/index")
 def index():
